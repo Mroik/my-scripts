9889e2a5fa8e3002722de59d32f98c8355b788b5
Mroik <mroik@delayed.space>
2025-05-03 03:49:10 +0200

    Change browser to firefox

--- random_xkcd.sh
+++ random_xkcd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -L "https://c.xkcd.com/random/comic/" 2>/dev/null | grep -o -P "(?<=>)https://imgs\.xkcd\.com/comics/.*\.png" | xargs brave
+curl -L "https://c.xkcd.com/random/comic/" 2>/dev/null | grep -o -P "(?<=>)https://imgs\.xkcd\.com/comics/.*\.png" | xargs firefox
